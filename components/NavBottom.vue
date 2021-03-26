cce6670e8b67a737e75949ec0f78944a672ba2d2
LuzTestai-hash <luz@bowerystudio.co>
2021-03-26 13:41:02 -0300

    scroll of navbar bottom

--- components/NavBottom.vue
+++ components/NavBottom.vue
@@ -85,38 +85,44 @@ export default {
     // we zenda
     const weZenda = document.querySelector('#we-zenda')
     const topWeZenda = weZenda.getBoundingClientRect().top
+    const bottomWeZenda = weZenda.getBoundingClientRect().bottom
     // what we do
     const WhatWeDo = document.querySelector('#what-we-do')
     const topWhatWeDo = WhatWeDo.getBoundingClientRect().top
+    const bottomWhatWeDo = WhatWeDo.getBoundingClientRect().bottom
     // methodology
     const methodology = document.querySelector('#methodology')
     const topMethodology = methodology.getBoundingClientRect().top
+    const bottomMethodology = methodology.getBoundingClientRect().bottom
     // clients
     const clients = document.querySelector('#clients')
     const topClients = clients.getBoundingClientRect().top
+    const bottomClients = clients.getBoundingClientRect().bottom
     // dashboard
     const dashboard = document.querySelector('#dashboard')
     const topDashboard = dashboard.getBoundingClientRect().top
-    // contact
-    const contact = document.querySelector('#contact')
-    const topContact = contact.getBoundingClientRect().top
+    const bottomDashboard = dashboard.getBoundingClientRect().bottom
+
     document.addEventListener('scroll', () => {
-      if (window.scrollY >= topWeZenda) {
+      if (window.scrollY >= topWeZenda && window.scrollY <= bottomWeZenda) {
         this.seccionSelected = 'we-zenda'
       }
-      if (window.scrollY >= topWhatWeDo) {
+      if (window.scrollY >= topWhatWeDo && window.scrollY <= bottomWhatWeDo) {
         this.seccionSelected = 'what-we-do'
       }
-      if (window.scrollY >= topMethodology) {
+      if (
+        window.scrollY >= topMethodology &&
+        window.scrollY <= bottomMethodology
+      ) {
         this.seccionSelected = 'methodology'
       }
-      if (window.scrollY >= topClients) {
+      if (window.scrollY >= topClients && window.scrollY <= bottomClients) {
         this.seccionSelected = 'clients'
       }
-      if (window.scrollY >= topDashboard) {
+      if (window.scrollY >= topDashboard && window.scrollY <= bottomDashboard) {
         this.seccionSelected = 'dashboard'
       }
-      if (window.scrollY >= topContact) {
+      if (window.scrollY > bottomDashboard % 2) {
         this.seccionSelected = 'contact'
       }
     })
@@ -144,15 +150,19 @@ export default {
     border-radius: 10px;
     height: 10vh;
     margin-bottom: 1rem;
+    transform: translateZ(0);
+    transition-delay: 1s;
     .section {
       margin-bottom: 0.5rem !important;
       margin-top: 0.5rem !important;
       padding: 0.6rem 1rem;
       cursor: pointer;
+      border-radius: 8px;
+      transition: all 0.2s cubic-bezier(8, 0, 0, 30);
+      transition: all 0.2s linear;
     }
     .active {
       background: #0a0a0b;
-      border-radius: 8px;
       color: #fff;
     }
   }
