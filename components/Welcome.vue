327ad0c213c9d6ac0bd17c88f7a77f6cab24514f
LuzTestai-hash <luz@bowerystudio.co>
2021-06-08 17:55:41 -0300

    pepe

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -3,7 +3,7 @@
     <div id="cursor" class="cursor" />
     <b-container class="welcome-container">
       <div class="container-title">
-        <h1 class="title">{{ $t('hero.title') }}</h1>
+        <h1 class="title">pepe</h1>
         <p class="subtitle">
           {{ $t('hero.description') }}
         </p>
